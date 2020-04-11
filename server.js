8916626c8261320d5450ea1eef2f59e6bfa16146
gvvashist9@gmail.com <gvvashist9@gmail.com>
2020-04-11 23:18:09 +0530

    db added

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const profile = require('./controller/profile')
 const db = knex({
     client: 'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: true,
        
     }
