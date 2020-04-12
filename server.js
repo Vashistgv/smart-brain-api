87fbc5fb5cc37d2d400b3c6a91e62d52b8d44fc2
gvvashist9@gmail.com <gvvashist9@gmail.com>
2020-04-12 17:34:52 +0530

    server ssl

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ const profile = require('./controller/profile')
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-       
+        connectionString: process.env.DATABASE_URL ,
+        ssl: true
     }
 })
 
