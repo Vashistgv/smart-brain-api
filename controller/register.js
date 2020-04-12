55469a1071c6c6c0687894cf795960b8bf5e248b
gvvashist9@gmail.com <gvvashist9@gmail.com>
2020-04-12 15:59:46 +0530

    test

--- controller/register.js
+++ controller/register.js
@@ -1,31 +1,42 @@
-const handleRegister = (req , res ,db ,bcrypt) => {
-    const {email , password , name} = req.body
+const handleRegister = (req, res, db, bcrypt) => {
+    const { email, password, name } = req.body
     const hash = bcrypt.hashSync(password);
-    console.log("this is bbbbbbbbbbbbbbbbbbbbbbbb",email , db )
+    console.log("this is bbbbbbbbbbbbbbbbbbbbbbbb", email)
+    db.insert({
+        hash: hash,
+        email: email
+    }).into('login').returning('email')
+        .then(LoginEmail => console.log("this issssssssssss ", LoginEmail))
+        .catch(err => console.log("this is errrrrrrrrrrrr"))
+
+
     db.transaction(trx => {
-        console.log("this is ",email)
-        trx.insert({
-            hash : hash ,
-            email : email
-        }).into('login')
-        .returning('email')
-        .then(LoginEmail => {
-            return  trx('users')
-            .returning('*')
-            .insert({
-                email : LoginEmail[0] ,
-                name  : name ,
-                joined : new Date ()
-            }).then(response => res.json(response[0]))
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
+        console.log("this is trxxxxxx ")
+
     })
-   .catch(err => res.status(400).json("user exist"))
-    
+        .catch(err => res.status(400).json("user exist"))
+
 
 }
 
 module.exports = {
-    handleRegister : handleRegister
+    handleRegister: handleRegister
 }
+
+
+// trx.insert({
+//     hash : hash ,
+//     email : email
+// }).into('login')
+// .returning('email')
+// .then(LoginEmail => {
+//     return  trx('users')
+//     .returning('*')
+//     .insert({
+//         email : LoginEmail[0] ,
+//         name  : name ,
+//         joined : new Date ()
+//     }).then(response => res.json(response[0]))
+// })
+// .then(trx.commit)
+// .catch(trx.rollback)
