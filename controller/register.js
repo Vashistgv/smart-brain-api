6488b1078630a0e22c9a5cf34262db945be4138f
gvvashist9@gmail.com <gvvashist9@gmail.com>
2020-04-12 15:08:41 +0530

    console

--- controller/register.js
+++ controller/register.js
@@ -1,7 +1,7 @@
 const handleRegister = (req , res ,db ,bcrypt) => {
     const {email , password , name} = req.body
     const hash = bcrypt.hashSync(password);
-    console.log("this is bbbbbbbbbbbbbbbbbbbbbbbb",email)
+    console.log("this is bbbbbbbbbbbbbbbbbbbbbbbb",email , db )
     db.transaction(trx => {
         console.log("this is ",email)
         trx.insert({
