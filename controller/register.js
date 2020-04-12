a9263a61959daedc7c20bf869a42c1ad599910f2
gvvashist9@gmail.com <gvvashist9@gmail.com>
2020-04-12 17:30:12 +0530

    err test

--- controller/register.js
+++ controller/register.js
@@ -2,26 +2,18 @@ const handleRegister = (req, res, db, bcrypt) => {
     const { email, password, name } = req.body
     const hash = bcrypt.hashSync(password);
     console.log("this is bbbbbbbbbbbbbbbbbbbbbbbb", email , db.select('*').from('users') , "dbbbbbbbbbbbbbbbbbbbbbbbbbbb")
-    db.insert({
-        hash: hash,
-        email: email
-    }).into('login').returning('email')
-        .then(LoginEmail => console.log("this issssssssssss ", LoginEmail))
-        .catch(err => console.log("this is errrrrrrrrrrrr"))
-
-
-    db.transaction(trx => {
-        console.log("this is trxxxxxx ")
-
-    })
+    // db.insert({
+    //     hash: hash,
+    //     email: email
+    // }).into('login').returning('email')
+    //     .then(LoginEmail => console.log("this issssssssssss ", LoginEmail))
+    //     .catch(err => console.log("this is errrrrrrrrrrrr"))
 
     db.select('*').from('users')
     .then(user => {
        console.log("user detailssssssssss" ,user)
     }) 
-    .catch(err => res.json("error in fetching users"))
-        .catch(err => res.status(400).json("user exist"))
-
+    .catch(console.log)
 
 }
 
@@ -29,7 +21,10 @@ module.exports = {
     handleRegister: handleRegister
 }
 
+// db.transaction(trx => {
+//     console.log("this is trxxxxxx ")
 
+// })
 // trx.insert({
 //     hash : hash ,
 //     email : email
